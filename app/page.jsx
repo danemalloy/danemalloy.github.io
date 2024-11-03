4b8b076fcc5063cc312bc5ed5cfbc7a6d4a3eecc
dane malloy <damalloy2@gmail.com>
2024-11-03 13:15:46 -0800

    fixed text issues

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ const Home = () => {
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hey, I'm <br /> <span className="text-accent">Dane Malloy</span>
+              Hey, I&apos;m <br /> <span className="text-accent">Dane Malloy</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               I am a certified computer scientist and professional Roblox game developer.
