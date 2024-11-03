d0230f445fd9adcc663d3de8401e41c009455844
dane malloy <damalloy2@gmail.com>
2024-11-03 14:27:40 -0800

    removed skills

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -137,7 +137,6 @@ const Resume = () => {
           <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
             <TabsTrigger value="experience">Experience</TabsTrigger>
             <TabsTrigger value="education">Education</TabsTrigger>
-            <TabsTrigger value="skills">Skills</TabsTrigger>
             <TabsTrigger value="about">About me</TabsTrigger>
           </TabsList>
 
