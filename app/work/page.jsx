934844ee1626d2d45adf7a6166949f58d9809357
dane malloy <damalloy2@gmail.com>
2024-11-05 14:46:14 -0800

    fixing image issues

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -127,7 +127,7 @@ const Work = () => {
               {projects.map((project, index) => {
                 return (
                   <SwiperSlide key={index} className="w-full">
-                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="h-[300px] relative group flex justify-center items-center bg-pink-50/20">
                       {/* overlay */}
                       <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       {/* image */}
